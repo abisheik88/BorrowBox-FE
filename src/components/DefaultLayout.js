9a46aee02968dca5b03058b19ead45a736adaa4c
abisheik88 <abishekvijayarangan@gmail.com>
2023-05-16 17:13:40 +0530

    user authentication

--- src/components/DefaultLayout.js
+++ src/components/DefaultLayout.js
@@ -1,14 +1,66 @@
 import React from 'react'
+import { Button, Col, Dropdown, Row } from 'antd';
+import { Link } from 'react-router-dom';
 
 function DefaultLayout(props) {
+    const user = JSON.parse(localStorage.getItem('user'))
+    const items = [
+        {
+            key: '1',
+            label: (
+                <a href="https://www.antgroup.com">
+                    Home
+                </a>
+            ),
+        },
+        {
+            key: '2',
+            label: (
+                <a href="https://www.antgroup.com">
+                    Bookings
+                </a>
+            ),
+        },
+        {
+            key: '3',
+            label: (
+                <a href="https://www.aliyun.com">
+                    Profile
+                </a>
+            ),
+        },
+        {
+            key: '4',
+            label: (
+                <li onClick={() => {
+                    localStorage.removeItem('user');
+                    window.location.href = '/login'
+                }}>LogOut</li>
+            ),
+        },
+    ];
     return (
         <div>
             <div className="header bs1">
-                <div className="d-flex justify-content-between">
+                <Row gutter={16} justify='center'>
+                    <Col lg={20} sm={24} xs={24}>
+                        <div className="d-flex justify-content-between">
 
-                    <h1>Borrow Box</h1>
+                            <h1>Borrow Box</h1>
 
-                </div>
+                            <Dropdown
+                                menu={{
+                                    items,
+                                }}
+                                placement="bottom"
+                            >
+                                <Button>{user.username}</Button>
+                            </Dropdown>
+
+                        </div>
+                    </Col>
+                </Row>
+
             </div>
             <div className="content">
                 {props.children}
